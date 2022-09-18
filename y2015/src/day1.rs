9b40c962cf07f2926f5d8628be96f465e6807348
Son Nguyen <anh.s.nguyen@gmail.com>
2022-09-18 01:28:20 -0700

    2015 - Day 1

---
+++ y2015/src/day1.rs
@@ -0,0 +1,63 @@
+use std::error::Error;
+use std::fs;
+
+pub fn execute() -> Result<(), Box<dyn Error>> {
+    println!("# Day 1");
+    let file = fs::read("./inputs/1.txt")?;
+    let file = &file[..];
+    println!("Part 1: {:?}", solve_part_1(file));
+    println!("Part 2: {:?}", solve_part_2(file));
+
+    Ok(())
+}
+
+fn solve_part_1(input: &[u8]) -> i64 {
+    input.iter().map(convert).sum()
+}
+
+fn convert(value: &u8) -> i64 {
+    match value {
+        40 => 1,
+        41 => -1,
+        _ => 0
+    }
+}
+
+fn solve_part_2(input: &[u8]) -> usize {
+    let mut floor = 0;
+    for (idx, i) in input.iter().enumerate() {
+        let value = convert(i);
+        floor += value;
+
+        if floor < 0 {
+            return idx + 1;
+        }
+    }
+
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn part_1_examples() {
+        assert_eq!(solve_part_1("(())".as_bytes()), 0);
+        assert_eq!(solve_part_1("()()".as_bytes()), 0);
+        assert_eq!(solve_part_1("(((".as_bytes()), 3);
+        assert_eq!(solve_part_1("(()(()(".as_bytes()), 3);
+        assert_eq!(solve_part_1("))(((((".as_bytes()), 3);
+        assert_eq!(solve_part_1("())".as_bytes()), -1);
+        assert_eq!(solve_part_1("))(".as_bytes()), -1);
+        assert_eq!(solve_part_1(")))".as_bytes()), -3);
+        assert_eq!(solve_part_1(")())())".as_bytes()), -3);
+    }
+
+    #[test]
+    fn part_2_examples() {
+        assert_eq!(solve_part_2(")".as_bytes()), 1);
+        assert_eq!(solve_part_2("()())".as_bytes()), 5);
+    }
+}
---
+++ y2015/src/main.rs
@@ -0,0 +1,8 @@
+use std::error::Error;
+use y2015::day1;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    day1::execute();
+
+    Ok(())
+}
