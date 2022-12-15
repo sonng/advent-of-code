6a7d427ad25794af62fae20954095f38fc6ccd19
Son Nguyen <anh.s.nguyen@gmail.com>
2022-12-15 20:59:53 +0700

    Massive refactor on day 15-1 to do part 2

--- y2022/src/day15.rs
+++ y2022/src/day15.rs
@@ -2,6 +2,7 @@ use std::{
     cmp::{max, min},
     collections::HashSet,
     fs,
+    ops::RangeInclusive,
 };
 
 use anyhow::Result;
@@ -9,9 +10,9 @@ use anyhow::Result;
 use crate::{Coord, Errors};
 
 pub fn exec() -> Result<()> {
-    let input = fs::read_to_string("./inputs/day15.txt")?;
-    // solve_part_1(&input)?;
-    solve_part_2(&input)?;
+    let input = fs::read_to_string("/Users/son/workspace/rust-aoc/y2022/inputs/day15.txt")?;
+    solve_part_1(&input)?;
+    // solve_part_2(&input)?;
     Ok(())
 }
 
@@ -21,17 +22,20 @@ fn solve_part_1(input: &str) -> Result<()> {
         .filter_map(|s| SensorDeployment::try_from(s).ok())
         .collect();
 
-    let mut other_points = HashSet::new();
-    deployments.iter().for_each(|d| {
-        other_points.insert(d.sensor);
-        other_points.insert(d.beacon);
-    });
+    let ranges: Vec<Option<RangeInclusive<isize>>> = deployments
+        .iter()
+        .map(|d| d.get_range_for_row(2000000))
+        .collect();
 
-    let spots = deployments.iter().flat_map(|d| d.get_row(2000000));
+    let ranges: Vec<RangeInclusive<isize>> = ranges.iter().filter_map(|f| f.clone()).collect();
+    let merged_ranges = merge_ranges(ranges);
 
-    let impossible: HashSet<Coord> = spots.filter(|s| !other_points.contains(s)).collect();
+    let number_of_spots: isize = merged_ranges
+        .iter()
+        .map(|r| (*r.start() - *r.end()).abs())
+        .sum();
 
-    println!("Day 15-1: {:?}", impossible.len());
+    println!("Day 15-1: {:?}", number_of_spots);
     Ok(())
 }
 
@@ -41,13 +45,23 @@ fn solve_part_2(input: &str) -> Result<()> {
         .filter_map(|s| SensorDeployment::try_from(s).ok())
         .collect();
 
+    let min_x = deployments.iter().map(|d| d.sensor.x).min().unwrap();
+    let min_y = deployments.iter().map(|d| d.sensor.y).min().unwrap();
+    let max_x = deployments.iter().map(|d| d.sensor.x).max().unwrap();
+    let max_y = deployments.iter().map(|d| d.sensor.y).max().unwrap();
+
     let all_spots: HashSet<Coord> = deployments.iter().flat_map(|d| d.get_diamond()).collect();
 
     let mut missed_spots = HashSet::new();
     for x in 0..20 {
         for y in 0..20 {
             let point = Coord::new(x as isize, y as isize);
-            if !all_spots.contains(&point) {
+            if !all_spots.contains(&point)
+                && point.x >= min_x
+                && point.x <= max_x
+                && point.y >= min_y
+                && point.y <= max_y
+            {
                 missed_spots.insert(point);
             }
         }
@@ -55,6 +69,42 @@ fn solve_part_2(input: &str) -> Result<()> {
 
     println!("Day 15-2: {:?}", missed_spots);
     Ok(())
+}
+
+fn merge_ranges(ranges: Vec<RangeInclusive<isize>>) -> Vec<RangeInclusive<isize>> {
+    let mut ranges = ranges;
+    ranges.sort_by(|a, b| {
+        let start_cmp = a.start().cmp(b.start());
+        let end_cmp = a.end().cmp(b.end());
+
+        if !start_cmp.is_eq() {
+            return start_cmp;
+        }
+
+        return end_cmp;
+    });
+
+    let mut merged_ranges: Vec<RangeInclusive<isize>> = vec![];
+    let mut idx = 0;
+    let mut cur = 1;
+
+    merged_ranges.push(ranges[idx].clone());
+    while cur < ranges.len() {
+        if ranges[cur].start() > merged_ranges[idx].end() {
+            merged_ranges.push(ranges[cur].clone());
+            idx += 1;
+            cur += 1;
+            continue;
+        }
+
+        let end = max(*merged_ranges[idx].end(), *ranges[cur].end());
+
+        merged_ranges[idx] = *merged_ranges[idx].start()..=end;
+
+        cur += 1;
+    }
+
+    merged_ranges
 }
 
 #[derive(Debug)]
@@ -120,6 +170,22 @@ impl SensorDeployment {
             .coords_for_diamond_row(self.find_size_of_diamond(), row)
     }
 
+    fn get_range_for_row(&self, row: isize) -> Option<RangeInclusive<isize>> {
+        let size = self.find_size_of_diamond();
+
+        if self.sensor.y + size < row || self.sensor.y - size > row {
+            return None;
+        }
+
+        let diff = (self.sensor.y - row).abs();
+        let units = (size - diff).abs();
+
+        Some(RangeInclusive::new(
+            self.sensor.x - units,
+            self.sensor.x + units,
+        ))
+    }
+
     fn get_diamond(&self) -> HashSet<Coord> {
         self.sensor.coords_for_diamond(self.find_size_of_diamond())
     }
@@ -133,9 +199,9 @@ impl SensorDeployment {
 
     fn find_size_of_diamond(&self) -> isize {
         if self.sensor.x == self.beacon.x {
-            (self.sensor.x - self.beacon.x).abs()
-        } else if self.sensor.y == self.beacon.y {
             (self.sensor.y - self.beacon.y).abs()
+        } else if self.sensor.y == self.beacon.y {
+            (self.sensor.x - self.beacon.x).abs()
         } else {
             let mut size = 0;
 
@@ -254,6 +320,46 @@ mod test {
 
         assert!(deloyment.within_range(Coord::new(6, 14)));
         assert!(!deloyment.within_range(Coord::new(5, 14)));
+    }
+
+    #[test]
+    fn test_get_diamond() {
+        let deployment = SensorDeployment {
+            sensor: Coord::new(10, 20),
+            beacon: Coord::new(10, 16),
+        };
+
+        assert_eq!(deployment.get_diamond().len(), 41);
+
+        let deployment = SensorDeployment {
+            sensor: Coord::new(14, 3),
+            beacon: Coord::new(15, 3),
+        };
+
+        assert_eq!(deployment.get_diamond().len(), 5);
+    }
+
+    #[test]
+    fn test_get_range_for_row() {
+        let deployment = SensorDeployment {
+            sensor: Coord::new(8, 7),
+            beacon: Coord::new(2, 10),
+        };
+
+        assert_eq!(deployment.get_range_for_row(10), Some(2..=14));
+        assert_eq!(deployment.get_range_for_row(11), Some(3..=13));
+        assert_eq!(deployment.get_range_for_row(16), Some(8..=8));
+        assert_eq!(deployment.get_range_for_row(7), Some(-1..=17));
+        assert_eq!(deployment.get_range_for_row(-2), Some(8..=8));
+        assert_eq!(deployment.get_range_for_row(17), None);
+        assert_eq!(deployment.get_range_for_row(-3), None);
+
+        let deployment = SensorDeployment {
+            sensor: Coord::new(16, 7),
+            beacon: Coord::new(15, 3),
+        };
+
+        assert_eq!(deployment.get_range_for_row(3), Some(15..=17));
     }
 
     #[test]
