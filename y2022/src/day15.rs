9c27df6a7125dfb64f5f6c52f9733a52632ef263
Son Nguyen <anh.s.nguyen@gmail.com>
2022-12-16 11:55:02 +0700

    REmoving some code

--- y2022/src/day15.rs
+++ y2022/src/day15.rs
@@ -51,7 +51,7 @@ fn solve_part_2(input: &str) -> Result<()> {
     let max_y = deployments.iter().map(|d| d.sensor.y).max().unwrap();
 
     let mut row_ranges: HashMap<isize, Vec<RangeInclusive<isize>>> = HashMap::new();
-    for i in 0..=4000000 {
+    for i in min_y..=max_y {
         let ranges: Vec<RangeInclusive<isize>> = deployments
             .iter()
             .filter_map(|d| d.get_range_for_row(i))
@@ -62,17 +62,6 @@ fn solve_part_2(input: &str) -> Result<()> {
             row_ranges.insert(i, ranges);
         }
     }
-
-    // let mut missed_spots = HashSet::new();
-    // for x in min_x..max_x {
-    //     for y in min_y..max_y {
-    //         if let Some(ranges) = row_ranges.get(&y) {
-    //             if !ranges.iter().any(|r| r.contains(&x)) {
-    //                 missed_spots.insert(Coord::new(x, y));
-    //             }
-    //         }
-    //     }
-    // }
 
     let key = row_ranges.keys().last().unwrap();
     let value = row_ranges.get(key).unwrap();
@@ -178,25 +167,6 @@ fn parse_coord(input: &str) -> Result<Coord, Errors> {
 }
 
 impl SensorDeployment {
-    fn minimum_coord(&self) -> Coord {
-        Coord::new(
-            min(self.sensor.x, self.beacon.x),
-            min(self.sensor.y, self.beacon.y),
-        )
-    }
-
-    fn maximum_coord(&self) -> Coord {
-        Coord::new(
-            max(self.sensor.x, self.beacon.x),
-            max(self.sensor.y, self.beacon.y),
-        )
-    }
-
-    fn get_row(&self, row: isize) -> HashSet<Coord> {
-        self.sensor
-            .coords_for_diamond_row(self.find_size_of_diamond(), row)
-    }
-
     fn get_range_for_row(&self, row: isize) -> Option<RangeInclusive<isize>> {
         let size = self.find_size_of_diamond();
 
@@ -213,108 +183,17 @@ impl SensorDeployment {
         ))
     }
 
-    fn get_diamond(&self) -> HashSet<Coord> {
-        self.sensor.coords_for_diamond(self.find_size_of_diamond())
-    }
-
-    fn within_range(&self, coord: Coord) -> bool {
-        let diamond_size = self.find_size_of_diamond();
-        let diamond_coords = self.sensor.coords_for_diamond(diamond_size);
-
-        diamond_coords.contains(&coord)
-    }
-
     fn find_size_of_diamond(&self) -> isize {
         if self.sensor.x == self.beacon.x {
             (self.sensor.y - self.beacon.y).abs()
         } else if self.sensor.y == self.beacon.y {
             (self.sensor.x - self.beacon.x).abs()
         } else {
-            let mut size = 0;
-
-            let direction = self.sensor.direction_towards(&self.beacon);
-
             let x_axis = (self.sensor.x - self.beacon.x).abs();
             let y_axis = (self.sensor.y - self.beacon.y).abs();
 
             y_axis + x_axis
-            // let mut coord = self.sensor;
-            // coord.x += direction.x;
-
-            // while !coord.is_on_the_diag(&self.beacon) {
-            //     coord.x += direction.x;
-            // }
-            // size = max(size, (self.sensor.x - coord.x).abs());
-
-            // let mut coord = self.sensor;
-            // coord.y += direction.y;
-            // while !coord.is_on_the_diag(&self.beacon) {
-            //     coord.y += direction.y;
-            // }
-            // size = max(size, (self.sensor.y - coord.y).abs());
-
-            // size
         }
-    }
-}
-
-impl Coord {
-    fn is_on_the_diag(&self, other: &Coord) -> bool {
-        (self.x - other.x).abs() == (self.y - other.y).abs()
-    }
-
-    fn coords_for_diamond_row(&self, size: isize, row: isize) -> HashSet<Coord> {
-        if (self.y < row && self.y + size < row) || (self.y > row && self.y - size > row) {
-            return HashSet::new();
-        }
-
-        let mut coords = HashSet::new();
-        for i in (0..=(size - (self.y - row).abs())).rev() {
-            coords.insert(Coord {
-                x: self.x - i,
-                y: row,
-            });
-            coords.insert(Coord {
-                x: self.x + i,
-                y: row,
-            });
-        }
-
-        coords
-    }
-
-    fn coords_for_diamond(&self, size_on_axis: isize) -> HashSet<Coord> {
-        let mut coords = HashSet::new();
-
-        // coords for the top
-        for j in 0..=size_on_axis {
-            for i in (0..=(size_on_axis - j)).rev() {
-                coords.insert(Coord {
-                    x: self.x - i,
-                    y: self.y + j,
-                });
-                coords.insert(Coord {
-                    x: self.x + i,
-                    y: self.y + j,
-                });
-            }
-        }
-
-        // coords for the bottom
-        for j in 0..=size_on_axis {
-            for i in (0..=(size_on_axis - j)).rev() {
-                coords.insert(Coord {
-                    x: self.x - i,
-                    y: self.y - j,
-                });
-                coords.insert(Coord {
-                    x: self.x + i,
-                    y: self.y - j,
-                });
-            }
-        }
-
-        coords
     }
 }
 
@@ -325,17 +204,6 @@ mod test {
     use super::SensorDeployment;
 
     #[test]
-    fn test_diamond_shape() {
-        let coord = Coord::new(5, 5);
-
-        let diamond = coord.coords_for_diamond(3);
-        assert_eq!(diamond.len(), 25);
-
-        let diamond = coord.coords_for_diamond(2);
-        assert_eq!(diamond.len(), 13);
-    }
-
-    #[test]
     fn test_find_diamond_size() {
         let deloyment = SensorDeployment {
             sensor: Coord::new(8, 7),
@@ -343,34 +211,6 @@ mod test {
         };
 
         assert_eq!(deloyment.find_size_of_diamond(), 9);
-    }
-
-    #[test]
-    fn test_within_range() {
-        let deloyment = SensorDeployment {
-            sensor: Coord::new(8, 7),
-            beacon: Coord::new(2, 10),
-        };
-
-        assert!(deloyment.within_range(Coord::new(6, 14)));
-        assert!(!deloyment.within_range(Coord::new(5, 14)));
-    }
-
-    #[test]
-    fn test_get_diamond() {
-        let deployment = SensorDeployment {
-            sensor: Coord::new(10, 20),
-            beacon: Coord::new(10, 16),
-        };
-
-        assert_eq!(deployment.get_diamond().len(), 41);
-
-        let deployment = SensorDeployment {
-            sensor: Coord::new(14, 3),
-            beacon: Coord::new(15, 3),
-        };
-
-        assert_eq!(deployment.get_diamond().len(), 5);
     }
 
     #[test]
@@ -404,30 +244,5 @@ mod test {
         };
 
         assert_eq!(deployment.get_range_for_row(13), Some(9..=15));
-    }
-
-    #[test]
-    fn test_get_coords_for_row() {
-        let coord = Coord::new(8, 7);
-
-        let coords = coord.coords_for_diamond_row(9, 16);
-        assert_eq!(coords.len(), 1);
-
-        let coords = coord.coords_for_diamond_row(9, 15);
-        assert_eq!(coords.len(), 3);
-
-        let coords = coord.coords_for_diamond_row(9, 5);
-        assert_eq!(coords.len(), 15);
-
-        let deloyment = SensorDeployment {
-            sensor: Coord::new(8, 7),
-            beacon: Coord::new(2, 10),
-        };
-
-        assert_eq!(deloyment.get_row(16).len(), 1);
-        assert!(deloyment.get_row(16).contains(&Coord::new(8, 16)));
-        assert!(deloyment.get_row(15).contains(&Coord::new(8, 15)));
-        assert!(deloyment.get_row(15).contains(&Coord::new(7, 15)));
-        assert!(deloyment.get_row(15).contains(&Coord::new(9, 15)));
     }
 }
